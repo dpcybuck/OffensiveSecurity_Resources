73686736763bcd72a837ca1cf3aec7a3647e7f34
dpcybuck <dpcybuck@gmail.com>
2018-12-17 12:56:55 -0600

    Create HackerSecretSanta.m
    A change that you could delete after you review the contents.  Merry Christmas - Hacker Santa

---
+++ HackerSecretSanta.m
@@ -0,0 +1,45 @@
+Infosystir named me your hacker secret santa.  You left your address off of the form.  Which I now have, and I will send you something to open.  
+Before I got the address i decided that your gift would be a donation to a charity in your name.  So, I donated to North Wind Farms.  
+I hope you like it.  I did it through a sight called greatnonprofits and North wind might get the check this week...but it might not be until
+next month.  Not sure.  https://greatnonprofits.freshdesk.com/support/solutions/articles/8000062861-how-would-i-know-if-the-nonprofit-already-received-my-donation-
+
+http://www.northwindfarm.org
+
+Here is the donation information I got back from the sight.  Its the closest thing I have that i can "give" you, showing there is a donation in your name.
+
+And like i said, you'll get an actual gift to open either this week or next.  Not because I delayed...just because it took me a while to go from 
+what you put on the form to infosystir and the actual address.
+
+I don't find myself in Texas much, and maybe I'll see you at a con in the Austion or San Antonio area.
+
+Have a merry christmas,
+David
+
+GreatNonprofits logo
+Thank you! You're making great things happen!
+
+Dear Len Noe,
+Thank you for your donation!
+
+Donation Date: December 13, 2018
+
+DONATIONS:
+
+Northwind Farm $100 
+GreatNonprofits $10 
+________________________________________ 
+TOTAL $110 
+Your confirmation ID is ch_1Dgzy5FBgMDCMAvo29HaJshYoU.
+GreatNonprofits processes donations on behalf of your organizations, so you will see our name on your credit card statement.
+
+Donations are mailed on a monthly basis to your organizations through our partner, JustGive.
+
+JustGive, a Donor Advised Fund, is the charitable organization of record. Their Tax ID, reflecting their nonprofit 501(c)(3) status, is 94-3331010. No goods or services were received in exchange for this donation.
+
+This email is a record of your donation. All or part of your donations may be tax deductible to the fullest extent of the law, please consult your financial or tax advisor.
+
+Thank you again for your support! If you have any questions, please visit our support center or contact us at support@greatnonprofits.org.
+
+Thanks,
+The GreatNonprofits Team
+https://greatnonprofits.org
